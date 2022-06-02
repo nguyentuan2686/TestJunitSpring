bd16c0972e7b5a33493ce5967674675cc055ddd1
Tuan. Nguyen Anh - CMC Global DU1.22 <natuan17@cmcglobal.vn>
2022-06-02 17:30:32 +0700

    add some function

---
+++ src/test/java/com/example/springboottesting/FormatTest.java
@@ -0,0 +1,16 @@
+package com.example.springboottesting;
+
+import org.junit.jupiter.api.Test;
+
+public class FormatTest {
+
+
+    @Test
+    public void given_when_then() {
+        //given - precondition or setup
+
+        //when - action or the behaviour that we are going test
+
+        //then - verify the output
+    }
+}
